7a22a2b025688148ddf958d1b6864141c72bf04b
Sviatoslav Kashyrin <mr.svyatikus@gmail.com>
2017-04-17 18:13:56 +0300

    validation form, fix small bugs

---
+++ app/add_project.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sviatoslav
+ * Date: 17.04.17
+ * Time: 15:06
+ */
+
+$name = $_POST['projectName'];
+
+$data = array();
+
+if ($name === '') {
+    $data['status'] = 'error';
+    $data['text'] = 'Заполните имя!';
+} else {
+    $data['status'] = 'success';
+    $data['text'] = 'Имя заполнено!';
+}
+
+header("Content-Type: application/json");
+echo json_encode($data);
+
+exit;
--- app/js/add_project.js
+++ app/js/add_project.js
@@ -10,14 +10,68 @@ var myModule = (function () {
 
     var _setUpListeners = function () {
         // Прослушка событий
-        $('#add-new-item').on('click', _showModal) // открыть модальное окно
+        $('#add-new-item').on('click', _showModal); // Поведение модального окна
+        $('#add-new-project').on('submit', _addProject); // Валидация формы
     };
+
 
     var _showModal = function (event) {
         console.log("Вызов модального окна");
         event.preventDefault();
-        $('add-project-popup').bPopup();
+
+        var divPopup = $('#add-project-popup'),
+            form = divPopup.find('.modal-form');
+
+        divPopup.bPopup({
+            speed: 450,
+            transition: 'slideIn',
+            transitionClose: 'slideBack',
+            onClose: function () {
+                console.log("Вы закрыли модальное окно");
+                form.find('.error-mes').text('').hide();
+            }
+        });
     };
+
+    var _addProject = function (event) {
+        // console.log("Добавление проекта");
+        event.preventDefault();
+        // Обьявляем переменные
+        var form = $(this),
+            url = 'add_project.php';
+            data = form.serialize();
+
+        // console.log(data);
+
+        // Запрос на сервер
+        $.ajax({
+            url : url,
+            type: 'POST',
+            dataType: 'json',
+            data: data
+        })
+            .done(function (answer) {
+                console.log(answer);
+                if(answer.status === 'success') {
+                    console.log(answer.text);
+                    form.find('.success-mes').text(answer.text).show();
+                } else {
+                    console.log(answer.text);
+                    form.find('.error-mes').text(answer.text).show();
+                }
+            })
+            .fail(function (answer) {
+                form.find('.error-mes').text("ERROR! Couldn't connect to the server!");
+                console.log("ERROR! Couldn't connect to the server!");
+
+            })
+    };
+
+    var _ajaxForm = function () {
+        // 1. Проверка формы
+        // 2, Сбор данных из формы
+        // 3. Отправка данных на сервер
+    }
 
     // Возвращает функцию
     return {
